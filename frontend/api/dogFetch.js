123c592befc2c76bda2b23ba9557f0f31454d2d2
Kirill Prostyakov <kprostyakov@gmail.com>
2017-09-16 16:55:09 +0300

    dog fetch task 2/5

---
+++ frontend/api/dogFetch.js
@@ -0,0 +1,32 @@
+import { handleErrors } from './handleErrors';
+const dogFetch = (dispatch, method, url, body = undefined) => {
+  dispatch({
+    status: Symbol('Loading')
+  });
+  return fetch(url, {
+    method,
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('jwt')
+    }),
+    body: JSON.stringify(body)
+  })
+      .then(handleErrors)
+      .then((response) => {
+        dispatch({
+          status: Symbol('Success'),
+          response
+        });
+        return Promise.resolve(response);
+      })
+      .catch((error) => { // { error: error }
+        console.log(error);
+        dispatch({
+          status: Symbol('Error'),
+          response: error.error
+        });
+        return Promise.reject(error);
+      });
+};
+
+export { dogFetch };
--- frontend/pages/courses_id/index.js
+++ frontend/pages/courses_id/index.js
@@ -1,4 +1,5 @@
-import { commonFetch } from '~/api/commonFetch';
+import  { dogFetch } from '~/api/dogFetch';
+import  { commonFetch } from '~/api/commonFetch';
 
 import { Header } from '~/components/Header';
 import { Footer } from '~/components/Footer';
@@ -16,20 +17,29 @@ class Page_courses_id extends React.Component {
   }
 
   state = {
-    speGetPage: {},
-    speDeleteProblem: {}
+    spe: {},
+    status: null,
+    response: null
   }
 
   componentDidMount = () =>
     commonFetch(
-      spe => this.setState({ speGetPage: spe }),
+      spe => this.setState({ spe }),
       'GET', `/api/pages/courses/${this.props.params.id}`
     )
 
-  deleteProblem = (event) =>
-    commonFetch((spe) => this.setState({ speDeleteProblem: spe }),
-      'DELETE', `/api/problems/${event.target.id}`
-    )
+  deleteProblem = (id) =>
+    dogFetch((request) => this.setState({ ...request }),
+      'DELETE', `/api/problems/${id}`
+    ).then(() => {
+      this.setState({ status: null });
+      const idFilter = (val) => !(val.id === id);
+      const newSpe = {
+        ...this.state.spe,
+        payload: this.state.spe.payload.filter(idFilter)
+      };
+      this.setState({ spe: newSpe });
+    })
 
   render = () =>
     <main className={css.main}>
@@ -38,7 +48,7 @@ class Page_courses_id extends React.Component {
       <div className="container">
         <CourseActions courseId={this.props.params.id} ifCourseDescriptionIsDisplayed/>
 
-        <Loading spe={this.state.speGetPage}>{(problems) =>
+        <Loading spe={this.state.spe}>{(problems) =>
           <section className="problems">
             {problems.map((problem) =>
               <div>
@@ -47,7 +57,13 @@ class Page_courses_id extends React.Component {
                   problemContent={problem.content}
                   problemType={problem.type}
                 />
-                <button id={problem.id} onClick={this.deleteProblem}>Remove</button>
+                { this.state.status ?
+                  <i className="fa fa-clock-o"/>
+                :
+                  <button className="button -black -fade-out-on-hover" onClick={() => this.deleteProblem(problem.id)}>
+                    Remove
+                  </button>
+                }
               </div>
             )}
           </section>
