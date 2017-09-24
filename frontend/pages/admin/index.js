6cb91e796c642ddbbd0714b1ab8503ac8f0238bd
Kirill Prostyakov <kprostyakov@gmail.com>
2017-09-24 23:51:39 +0300

    add User component into Page_admin

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,44 +1,27 @@
 import  { dogFetch } from '~/api/dogFetch';
+import  { User } from './components/User.js';
 
 class Page_admin extends React.Component {
 
   state = {
-    users: [],
-    status: null,
-    response: null
+    users: []
   }
 
   componentDidMount = () =>
     dogFetch(
-      (request) => this.setState({ ...request }),
+      (res) => this.setState({ users: res.response }),
       'GET', '/api/users/'
-    ).then(() => {
-      this.setState({
-        users: this.state.status.toString() === 'Symbol(Success)' ? this.state.response : [],
-        status: null
-      });
-    })
+    )
 
-  deleteUser = (id) =>
-    dogFetch((request) => this.setState({ ...request }),
-    'DELETE', `/api/users/${id}`
-  ).then(() => {
-    this.setState({ status: null });
+  filterUsers = (id) => {
     const idFilter = (val) => !(val.id === id);
     this.setState({ users: this.state.users.filter(idFilter) });
-  })
+  }
 
   render = () =>
     <ul>
       {this.state.users.map((user) =>
-        <li key={user.id}>
-          { user.username }
-          { this.state.status ?
-            <i className="fa fa-clock-o"/>
-          :
-            <button onClick={() => this.deleteUser(user.id)}>Remove</button>
-          }
-        </li>
+        <User user={user} cb={this.filterUsers}/>
       )}
     </ul>
 }
